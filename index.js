316bdae6402c6f7ff22eb1c9e7f27ffa730f8f77
apinantest <56676897+apinantest@users.noreply.github.com>
2020-02-03 16:36:30 +0700

    support eth

---
+++ index.js
@@ -0,0 +1,31 @@
+const fetch = require('node-fetch');
+const { URLSearchParams } = require('url');
+ 
+
+var url = 'https://api.bitkub.com/api/market/ticker';
+var token = 't8ZmtNfbwoeTqA0PXOq4fyn6geIHiqwZ2OC6GTREaHs'
+fetch(url)
+.then(res => res.json())
+.then(res => {
+    console.log( res.THB_ETH.last )
+    line_notify("Last: "+res.THB_ETH.last+"\nHight: "+res.THB_ETH.highestBid+"\nLow: "+res.THB_ETH.lowestAsk+"\nChange: "+res.THB_ETH.change)
+});
+
+line_notify = (msg) => {
+    const params = new URLSearchParams();
+    var d = new Date();
+    params.append('message', "Etherium state "+d+"\n" + msg);
+
+    fetch('https://notify-api.line.me/api/notify',
+    {
+        method: 'post',
+        body:    params,
+        headers: { 
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Bearer '+token
+        },
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
